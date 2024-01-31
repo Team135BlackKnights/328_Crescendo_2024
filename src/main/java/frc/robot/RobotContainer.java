627959a73eeb3a0f091f8760f0771eecbb3fa0e1
tornadolehto <llehtomaki266@phm.k12.in.us>
2024-01-31 15:31:57 -0500

    removed more unused stuff
    &&&&&&&

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.MecanumSub;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.XboxController;
 
@@ -18,7 +17,7 @@ import edu.wpi.first.wpilibj.XboxController;
 public class RobotContainer {
   // The robot's subsystems and commands are 
 
-  private final MecanumSub mecanumSub = new MecanumSub();
+  
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
 
