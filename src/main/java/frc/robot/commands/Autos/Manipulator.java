6b0f49114bc3649363f96ca43218cb2e65251339
Leeee Lehtomaki <153555185+tornadolehto@users.noreply.github.com>
2024-02-20 15:58:19 -0500

    addedtoMain

---
+++ src/main/java/frc/robot/commands/Autos/Manipulator.java
@@ -0,0 +1,56 @@
+package frc.robot.commands.Autos;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.LiftIntakeS;
+
+public class Manipulator extends Command {
+boolean m_goDown;
+boolean isFinished;
+
+    public final LiftIntakeS lift;
+    
+    public Manipulator(LiftIntakeS liftS,boolean goDown){
+        //when refering to desired time you can also say "seconds"
+        //when saying intake you are refering to the subsystem intakeS
+        lift = liftS;
+        m_goDown = goDown;
+        //when saying the word lift you are refering  to the subsystem liftS
+        //when saying the word "autoSpeed" you are refering to the motor Speed
+        addRequirements(liftS);
+        
+    }
+    @Override
+
+
+    public void initialize(){
+    lift.resetEncoders();
+    isFinished = false;    
+        SmartDashboard.putString("Current Action", getName());
+
+    }
+    @Override
+    public void execute() {
+        if(m_goDown){
+            if(Math.abs(lift.getLiftIntakeEncoderAverage()) < .1){ //will need to be changed, but I aint risking the motors eating themselves.
+                lift.spinLiftIntake(0.25);
+            }else{
+                isFinished = true;
+            }
+        } else {if(Math.abs(lift.getLiftIntakeEncoderAverage()) < .1){ //will need to be changed, but I aint risking the motors eating themselves.
+                lift.spinLiftIntake(-0.25);
+            }else{
+                isFinished = true;
+            }
+        }
+       // LiftIntakeS.liftIntakeMotor2.setIdleMode(IdleMode.kCoast);
+    }
+    @Override
+    public void end(boolean interrupted) {
+        lift.stopLiftIntake();
+    }
+    @Override
+    public boolean isFinished() {
+        return isFinished;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/MecanumSub.java
@@ -0,0 +1,147 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+import edu.wpi.first.math.kinematics.MecanumDriveOdometry;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelPositions;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.kauailabs.navx.frc.AHRS;
+//import com.revrobotics.CANSparkMax;
+//import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkBase.IdleMode;
+
+
+
+  //its the motors gang
+  public class MecanumSub extends SubsystemBase {
+  public TalonFX frontLeft = new TalonFX(Constants.FRONT_LEFT_MOTOR);
+  public TalonFX frontRight = new TalonFX(Constants.FRONT_RIGHT_MOTOR);
+  public TalonFX backRight = new TalonFX(Constants.BACK_RIGHT_MOTOR);
+  public TalonFX backLeft = new TalonFX(Constants.BACK_LEFT_MOTOR); 
+
+  public MecanumDrive drive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
+  
+  Translation2d m_frontLeftLocation = new Translation2d(Units.inchesToMeters(19.1), Units.inchesToMeters(19.1)); // check to see if 19.1 is the correct length in inches, but we're pretty sure it is from doing math stuff
+  Translation2d m_frontRightLocation = new Translation2d(Units.inchesToMeters(19.1), -Units.inchesToMeters(19.1));
+  Translation2d m_backLeftLocation = new Translation2d(-Units.inchesToMeters(19.1), Units.inchesToMeters(19.1));
+  Translation2d m_backRightLocation = new Translation2d(-Units.inchesToMeters(19.1), -Units.inchesToMeters(19.1));
+
+  private final Encoder m_frontLeftEncoder = new Encoder(0,1);
+  private final Encoder m_frontRightEncoder = new Encoder(2,3);
+  private final Encoder m_backLeftEncoder = new Encoder(4,5);
+  private final Encoder m_backRightEncoder = new Encoder(6,7);
+
+
+private final AHRS m_gyro = new AHRS(Port.kUSB1);
+  // creation of kinematics with utilization of wheel locations
+ MecanumDriveKinematics m_kinematics = new MecanumDriveKinematics
+    (m_frontRightLocation, m_frontLeftLocation, m_backRightLocation, m_backLeftLocation);
+ MecanumDriveWheelSpeeds m_speeds = new MecanumDriveWheelSpeeds(0,0,0,0);
+
+  Pose2d m_pose = new Pose2d();
+
+  // kinematics and odometry
+  MecanumDriveOdometry m_odometry = new MecanumDriveOdometry(
+  m_kinematics,
+  m_gyro.getRotation2d(),
+  new MecanumDriveWheelPositions(
+    m_frontLeftEncoder.getDistance(), m_frontRightEncoder.getDistance(),
+    m_backLeftEncoder.getDistance(), m_backRightEncoder.getDistance()
+  ),
+  new Pose2d(5.0, 13.5, new Rotation2d())
+);
+
+public void periodic() {
+  // Get my wheel positions
+  var wheelPositions = new MecanumDriveWheelPositions(
+    m_frontLeftEncoder.getDistance(), m_frontRightEncoder.getDistance(),
+    m_backLeftEncoder.getDistance(), m_backRightEncoder.getDistance());
+
+  // Get the rotation of the robot from the gyro.
+  var gyroAngle = m_gyro.getRotation2d();
+
+  // Update the pose
+  m_pose = m_odometry.update(gyroAngle, wheelPositions);
+}
+
+    public void runStop() {
+    //Sets motors to stop when not in use
+        frontLeft.setNeutralMode(NeutralModeValue.Brake);
+        frontRight.setNeutralMode(NeutralModeValue.Brake);
+        backLeft.setNeutralMode(NeutralModeValue.Brake);
+        backRight.setNeutralMode(NeutralModeValue.Brake); 
+    }
+
+  public MecanumSub() {
+
+  }
+  public double[] calculateDistance(boolean isStrafe) {
+    if (isStrafe){
+        double averageEncoderCountsFLBR = (frontLeft.getPosition().getValueAsDouble() + backRight.getPosition().getValueAsDouble()) / 2.0;
+        double averageEncoderCountsFRBL = (frontRight.getPosition().getValueAsDouble() + backLeft.getPosition().getValueAsDouble()) / 2.0;
+        double averageEncoderCountsOverall = (Math.abs(averageEncoderCountsFLBR) + Math.abs(averageEncoderCountsFRBL)) / 2.0;
+        // Convert encoder counts to wheel rotations
+        double rotationsFLBR = averageEncoderCountsFLBR / Constants.AutoConstants.ENCODER_COUNTS_PER_REV;
+        double rotationsFRBL = averageEncoderCountsFRBL / Constants.AutoConstants.ENCODER_COUNTS_PER_REV;
+        double rotationsOverall = averageEncoderCountsOverall / Constants.AutoConstants.ENCODER_COUNTS_PER_REV;
+        double[] distanceTraveled = new double[2];
+        distanceTraveled[0] = rotationsFLBR * Constants.AutoConstants.WHEEL_CIRCUMFERENCE;
+        distanceTraveled[1] = rotationsFRBL * Constants.AutoConstants.WHEEL_CIRCUMFERENCE;
+        distanceTraveled[2] = rotationsOverall * Constants.AutoConstants.WHEEL_CIRCUMFERENCE;
+        // Calculate distance traveled
+        return distanceTraveled;
+    }else{
+    double averageEncoderCounts = (frontLeft.getPosition().getValueAsDouble() + 
+                                   frontRight.getPosition().getValueAsDouble() + 
+                                   backLeft.getPosition().getValueAsDouble() + 
+                                   backRight.getPosition().getValueAsDouble()) / 4.0;
+
+    // Convert encoder counts to wheel rotations
+    double rotations = averageEncoderCounts / Constants.AutoConstants.ENCODER_COUNTS_PER_REV;
+
+    // Calculate distance traveled
+    double[] distanceTraveled = new double[1];
+    distanceTraveled[0] = rotations * Constants.AutoConstants.WHEEL_CIRCUMFERENCE;
+    return distanceTraveled;}
+    }
+    public double getYaw() {
+      return m_gyro.getYaw();
+    }
+    public void resetEncoders() {
+      frontLeft.setPosition(0);
+      frontRight.setPosition(0);
+      backLeft.setPosition(0);
+      backRight.setPosition(0);
+  }
+  // sets wheel speeds
+   public MecanumSub(ChassisSpeeds speeds) {
+    m_speeds = m_kinematics.toWheelSpeeds(speeds);
+    //frontLeft.set(0.5);
+    }
+
+  public void MecanumDrive(double frontLeft,double backLeft,double frontRight,double backRight) {
+      MecanumDrive(frontLeft, backLeft, frontRight, backRight);
+  }
+
+
+
+  }
