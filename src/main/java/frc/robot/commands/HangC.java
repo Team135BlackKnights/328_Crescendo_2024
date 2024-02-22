5350504345e707113a35713fae3eb8aef1d5e8e5
Leeee Lehtomaki <153555185+tornadolehto@users.noreply.github.com>
2024-02-22 17:09:02 -0500

    something
    hang done

---
+++ src/main/java/frc/robot/commands/HangC.java
@@ -0,0 +1,47 @@
+package frc.robot.commands;
+
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.HangS;
+
+/** An example command that uses an example subsystem. */
+
+public class HangC extends Command {
+    private final HangS hangS;
+    
+    public HangC(HangS subsystem) {
+        hangS = subsystem;
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+
+        if (RobotContainer.m_operatorController.getYButtonPressed()) {
+           hangS.pneumaticOut();
+        }   else if (RobotContainer.m_operatorController.getXButtonPressed()) {
+           hangS.pneumaticIn();
+        }
+    }
+
+
+    @Override
+    public void end(boolean interrupted) {
+    
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+}
---
+++ src/main/java/frc/robot/subsystems/HangS.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+//API is like a book. we IMPORT "chapters" of this book, to then use their knowledge.
+//Imagine our .command.CommandBase saying "we're in this chapter! use this data."
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase; //duh
+import frc.robot.Constants;
+
+public class HangS extends SubsystemBase { //create a subsystem
+public static DoubleSolenoid hang = new DoubleSolenoid(2, PneumaticsModuleType.REVPH, Constants.Hang_ID, Constants.Hang_ID2);
+
+    public HangS(){
+
+    }
+
+    public void pneumaticOut() {
+     hang.set(DoubleSolenoid.Value.kForward);
+      }
+
+    public void pneumaticIn() {
+     hang.set(DoubleSolenoid.Value.kReverse);
+      }
+
+    }
